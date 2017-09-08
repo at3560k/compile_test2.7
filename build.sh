c61e2edd3ed8543b439f26f68d83658a964469c6
John Q Doe <bitbucket.org@maelstrm.cotse.net>
2017-09-07 23:23:55 -0400

    workarounds that make no sense...

--- build.sh
+++ build.sh
@@ -2,4 +2,10 @@
 
 #pyinstaller compiletest/compiletest.py
 #pyinstaller compile_test.spec
-pyinstaller --add-data 'compile_test/resource.txt:.' compile_test/compile_test.py
+
+# the below, res('resource', 'resource.txt') works
+#pyinstaller --add-data 'compile_test/resource.txt:.' compile_test/compile_test.py
+
+
+# the below, res('resource', 'fubar/resource.txt') works
+pyinstaller --add-data 'compile_test/resource.txt:fubar' compile_test/compile_test.py
